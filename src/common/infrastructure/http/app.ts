e9d1a00d7568acc85d61944de04633ef203f5bb6
FernandoROL <felhopez@gmail.com>
2024-11-22 16:59:33 -0300

    project creation and database setup

---
+++ src/common/infrastructure/http/app.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from 'cors';
+import { routes } from "./routes";
+import { errorHandler } from "./middleware/errorHandler";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API documentation',
+        version: '1.0.0'
+      },
+    },
+    apis: [],
+}
+
+const swaggerSpecs = swaggerJSDoc(options)
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs))
+app.use(routes)
+app.use(errorHandler)
+
+export { app }
---
+++ src/common/infrastructure/http/server.ts
@@ -0,0 +1,15 @@
+import { dataSource } from "../typeorm";
+import { app } from "./app";
+import { env } from "process";
+
+dataSource.initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log('Server is running on port: ' + env.PORT)
+      console.log('\x1b[4m\x1b[33m%s\x1b[0m', `Access at: ${env.API_URL}`);
+      console.log('\x1b[36m%s\x1b[0m', `API docs avaliable at ${env.API_URL}/docs`)
+    })
+  })
+  .catch((error) => {
+    console.log('Error initializing data source: ' +error)
+  })
