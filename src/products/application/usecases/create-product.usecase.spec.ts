4c86b599ead631e0ae90da84d618e3c5d701e1e6
FernandoROL <felhopez@gmail.com>
2024-12-02 17:32:42 -0300

    create product usercase tests & controller

--- src/products/application/usecases/create-product.usecase.spec.ts
+++ src/products/application/usecases/create-product.usecase.spec.ts
@@ -1,5 +1,70 @@
+import { ProductsRepository } from "@/products/domain/repositories/products.repository"
 import { CreateProductUseCase } from "./create-product.usecase"
+import { ProductsInMemoryRepository } from "@/products/infrastructure/in-memory/repositories/products-in-memory.repository"
+import { ConflictError } from "@/common/domain/error/conflict-error"
+import { BadRequestError } from "@/common/domain/error/bad-request-error"
 
 describe('CreateProductUseCase Unit Test', () => {
-  sut: CreateProductUseCase.UseCase
+  let sut: CreateProductUseCase.UseCase
+  let repository: ProductsRepository
+
+  beforeEach(() => {
+    repository = new ProductsInMemoryRepository()
+    sut = new CreateProductUseCase.UseCase(repository)
+  })
+
+  it('Should create a product', async () => {
+    const spyInsert = jest.spyOn(repository, 'insert')
+    const props = {
+      name: 'Product 1',
+      price: 10, 
+      quantity: 6
+    }
+
+    const result  = await sut.execute(props)
+    expect(result.id).toBeDefined()
+    expect(result.created_at).toBeDefined()
+    expect(spyInsert).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not be possible to register a product with the name of another product', async () => {
+    const props = {
+      name: 'Product 1',
+      price:  20,
+      quantity: 5
+    }
+
+    await sut.execute(props)
+    await expect(sut.execute(props)).rejects.toBeInstanceOf(ConflictError)
+  })
+
+  it('should throw error when name not provided', async () => {
+    const props = {
+      name: null,
+      price:  20,
+      quantity: 5
+    }
+
+    await expect(sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
+
+  it('should throw error when price not provided', async () => {
+    const props = {
+      name: "Product 1",
+      price:  null,
+      quantity: 5
+    }
+
+    await expect(sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
+
+  it('should throw error when quantity not provided', async () => {
+    const props = {
+      name: 'Product 1',
+      price:  20,
+      quantity: null
+    }
+
+    await expect(sut.execute(props)).rejects.toBeInstanceOf(BadRequestError)
+  })
 })
---
+++ src/products/infrastructure/http/create-product.controller.ts
@@ -0,0 +1,39 @@
+import { AppError } from "@/common/domain/error/app-error";
+import { Request, Response } from "express";
+import { z } from "zod";
+import { ProductsTypeormRepository } from "../typeorm/repositories/products-typeorm.repository";
+import { dataSource } from "@/common/infrastructure/typeorm";
+import { Product } from "../typeorm/entities/products.entity";
+import { CreateProductUseCase } from "@/products/application/usecases/create-product.usecase";
+
+export async function createProductController(request: Request, response: Response) {
+  const createProductBodySchema = z.object({
+    name: z.string(),
+    price: z.number(),
+    quantity: z.number()
+  })
+
+  const validateData = createProductBodySchema.safeParse(request.body)
+
+  if (validateData.success === false) {
+    console.log("Invalid data", validateData.error.format())
+    throw new AppError(`${validateData.error.errors.map(error => {
+      return `${error.path} -> ${error.message}`
+    })}`,
+    )
+  }
+
+  const {name,price,quantity} = validateData.data
+
+  const repository = new ProductsTypeormRepository
+  repository.productsRepository = dataSource.getRepository(Product)
+  const createProductUseCase = new CreateProductUseCase.UseCase(repository)
+
+  const product = await createProductUseCase.execute({
+    name,
+    price,
+    quantity
+  })
+
+  return response.status(201).json(product)
+}
