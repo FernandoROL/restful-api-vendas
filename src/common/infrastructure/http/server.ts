771746dbd2292d627e2a8dc62a36df53746771e2
FernandoROL <felhopez@gmail.com>
2025-03-03 14:58:31 -0300

    display link fix

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -7,8 +7,8 @@ dataSource.initialize()
   .then(() => {
     app.listen(env.PORT, () => {
       console.log('Server is running on port: ' + env.PORT)
-      console.log('\x1b[4m\x1b[33m%s\x1b[0m', `Access at: ${env.API_URL}`);
-      console.log('\x1b[36m%s\x1b[0m', `API docs avaliable at ${env.API_URL}/docs`)
+      console.log('\x1b[4m\x1b[33m%s\x1b[0m', `Access at: http://localhost:${env.PORT}`);
+      console.log('\x1b[36m%s\x1b[0m', `API docs avaliable at http://localhost:${env.PORT}/docs`)
     })
   })
   .catch((error) => {
